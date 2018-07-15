862ad52e545fe482fb49976ab4ef7f80a7bfa9bf
emmax <maxkanold@freenet.de>
2018-07-15 19:56:59 +0200

    implemented pivotRandom

--- PivotAlgorithms.cpp
+++ PivotAlgorithms.cpp
@@ -1,7 +1,20 @@
+#include <random>
+#include <time.h>
+
 #include "PivotAlgorithms.h"
 
 size_t pivotRandom(const std::vector<Circle>& circles) {
-    return circles.size();
+    std::vector<size_t> negCircles;
+    for (size_t i = 0; i < circles.size(); i++) {
+        if (circles[i].costPerFlow < 0) {negCircles.push_back(i);}
+    }
+    if (negCircles.empty()) {return circles.size();}
+
+    std::mt19937 rng;
+    rng.seed(static_cast<long unsigned int>(time(0)));
+    std::uniform_int_distribution<size_t> rand(0, negCircles.size() - 1);
+
+    return negCircles[rand(rng)];
 }
 
 size_t pivotMaxVal(const std::vector<Circle>& circles) {
